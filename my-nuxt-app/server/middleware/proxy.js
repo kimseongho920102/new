c384e5c142ed6a293a89bff81ff68fbff728f428
seong_ho_kim <92960654+kimseongho920102@users.noreply.github.com>
2025-01-06 07:21:48 +0000

    test

--- my-nuxt-app/server/middleware/proxy.js
+++ my-nuxt-app/server/middleware/proxy.js
@@ -1,9 +1,10 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import { fromNodeMiddleware } from 'h3';
 
-export default function (req, res, next) { if (req.url.startsWith('/api')) {
-     createProxyMiddleware({
-         target: 'http://34.64.157.30:4000',
-         pathRewrite: { '^/api': '' },
-         changeOrigin: true 
-        })(req, res, next); 
-    } else { next(); }}
+export default fromNodeMiddleware(
+  createProxyMiddleware('/api', {
+    target: 'http://34.64.157.30:4000',
+    pathRewrite: { '^/api': '' },
+    changeOrigin: true
+  })
+);
