79475e0752332eee7abce6b21b00217479d8b3bb
seong_ho_kim <92960654+kimseongho920102@users.noreply.github.com>
2025-01-06 06:34:03 +0000

    test

--- my-nuxt-app/server/middleware/proxy.js
+++ my-nuxt-app/server/middleware/proxy.js
@@ -1,23 +1,9 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const proxy = createProxyMiddleware({
-  target: 'http://34.64.157.30:4000', // API 서버 URL
-  changeOrigin: true, // CORS 회피
-  pathRewrite: { '^/api': '' }, // `/api`로 시작하는 경로를 제거
-});
-
-export default defineEventHandler((event) => {
-  // Nuxt 3의 이벤트 핸들러에서 요청/응답 흐름 연결
-  const { req, res } = event.node;
-
-  return new Promise((resolve, reject) => {
-    proxy(req, res, (err) => {
-      if (err) {
-        console.error('Proxy Error:', err);
-        reject(err); // 에러가 발생하면 Promise를 reject
-      } else {
-        resolve(); // 성공적으로 처리된 경우 Promise를 resolve
-      }
-    });
-  });
-});
+export default function (req, res, next) { if (req.url.startsWith('/api')) {
+     createProxyMiddleware({
+         target: 'http://34.64.157.30:4000',
+         pathRewrite: { '^/api': '' },
+         changeOrigin: true 
+        })(req, res, next); 
+    } else { next(); }}
